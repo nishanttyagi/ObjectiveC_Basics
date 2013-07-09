6a4ce87a6f7c31fa4bd5f550012bb9fcb04fd1d8
Nishant Tyagi <nishanttyagi1023@gmail.com>
2013-07-09 17:17:07 +0530

    I have changed my class

--- ObjectiveC_Basics/ObjectiveC_Basics/ViewController.m
+++ ObjectiveC_Basics/ObjectiveC_Basics/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // I am modifying view did load
 }
 
 - (void)didReceiveMemoryWarning
