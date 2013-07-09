6b9f832c95642fffcae747f8334ec4fd40cdd54a
Nishant Tyagi <nishanttyagi1023@gmail.com>
2013-07-09 15:28:40 +0530

    Created Project

---
+++ ObjectiveC_Basics/ObjectiveC_Basics/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ObjectiveC_Basics
+//
+//  Created by Nishant Tyagi on 7/9/13.
+//  Copyright (c) 2013 Nishant Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ObjectiveC_Basics/ObjectiveC_Basics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ObjectiveC_Basics
+//
+//  Created by Nishant Tyagi on 7/9/13.
+//  Copyright (c) 2013 Nishant Tyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
